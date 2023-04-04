afc2fa716cd8fffddfc97b56fd4ddaa1343cfd53
Rajendra Maharjan <3464136+RajendraMaharjan@users.noreply.github.com>
2023-04-04 13:42:15 -0500

    start spring io initial project push for lab 1

---
+++ src/main/java/edu/miu/waalab/WaalabApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.waalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaalabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaalabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/miu/waalab/WaalabApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.waalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaalabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
